48d7dd00f00b77c56dcc6b04f32daaf6461df951
Douwe Hoekstra <douwehoekstra2512@gmail.com>
2022-10-07 22:14:58 +0200

    Add cleardb command to manage script

--- src/manage.py
+++ src/manage.py
@@ -2,10 +2,16 @@ import click
 from passlib.hash import pbkdf2_sha256
 
 from tavern_db.database import db_session
-from tavern_db.models import User, UserGroup
+from tavern_db.models import Permission, User, UserGroup
 
 
-def main():
+@click.group()
+def cli():
+    pass
+
+
+@click.command(help="Initialize server with superuser")
+def init():
     username = click.prompt("Username for superuser", type=str)
     password = click.prompt("Password for superuser", type=str)
 
@@ -20,5 +26,16 @@ def main():
     click.echo("Database configured!")
 
 
+@click.command(help="Remove all records from the database")
+def cleardb():
+    User.query.delete()
+    UserGroup.query.delete()
+    Permission.query.delete()
+    db_session.commit()
+
+
+cli.add_command(init)
+cli.add_command(cleardb)
+
 if __name__ == "__main__":
-    main()
+    cli()
